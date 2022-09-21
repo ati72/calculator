2c3e55901303292f35a8495f0265395f1f4134b4
ati72 <hegedusattila@protonmail.com>
2022-09-21 19:04:44 +0200

    working on calc functions

--- script.js
+++ script.js
@@ -24,10 +24,42 @@ function operate(operator, a, b) {
 }
 
 const display = document.querySelector('.display');
+const displayCurrentOperand = document.querySelector('.current-operand');
+const displayPreviousOperand = document.querySelector('.prev-operand');
 const btnNumbers = document.querySelectorAll('.number');
 const btnOperators = document.querySelectorAll('.operator');
 const btnEquals = document.querySelector('.equals');
 const btnClear = document.querySelector('.clear');
 
-btnNumbers.forEach(btn => btn.addEventListener('click', () => display.innerHTML += btn.innerHTML));
-btnClear.addEventListener('click', () => display.innerHTML = '');
+let currentOperand;
+let operator;
+let previousOperand;
+
+btnClear.addEventListener('click', () => {
+    displayCurrentOperand.innerHTML = '';
+    displayPreviousOperand.innerHTML = '';
+    currentOperand = null;
+    previousOperand = null;
+    operator = null;
+});
+
+btnNumbers.forEach(btn => btn.addEventListener('click', () => {
+    if(displayCurrentOperand.innerHTML === '0' && btn.innerHTML === '0') return;
+    if(displayCurrentOperand.innerHTML === '0' && btn.innerHTML !== '0') displayCurrentOperand.innerHTML = '';
+    displayCurrentOperand.innerHTML += btn.innerHTML;
+    currentOperand = parseInt(displayCurrentOperand.innerHTML);
+}));
+
+btnOperators.forEach(btn => btn.addEventListener('click', () => {
+    operator = btn.innerHTML;
+    previousOperand = currentOperand;
+    displayPreviousOperand.innerHTML += currentOperand;
+    displayPreviousOperand.innerHTML += operator;
+    currentOperand = null;
+    displayCurrentOperand.innerHTML = '';
+}));
+
+btnEquals.addEventListener('click', () => {
+    displayCurrentOperand.innerHTML = operate(operator, previousOperand, currentOperand);
+    displayPreviousOperand.innerHTML += currentOperand;
+})
